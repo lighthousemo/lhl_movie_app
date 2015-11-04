cda8ecdda6bf00614d529f5a1c01509e33cdd9ff
Monica Olinescu <monica.olinescu@gmail.com>
2015-11-04 11:32:11 -0800

    Live code from class

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,17 +1,35 @@
 $(document).ready(function() {
   var mainPageEl = $("#main-page");
 
-  var movies = [
-    {id: 1, title: "Titanic", poster_url: "http://t0.gstatic.com/images?q=tbn:ANd9GcQhYjUIu2o5v5u3rfJpCq5Cz0Q9WK--XdYxai_N2d0ImohPiIOp", synopsis: "James Cameron's Titanic is an epic, action-packed romance set against the ill-fated maiden voyage of the R.M.S. Titanic; the pride and joy of the White Star Line and, at the time, the largest moving object ever built.", year: 2011},
-    {id: 2, title: "Other Movie", poster_url: "http://t0.gstatic.com/images?q=tbn:ANd9GcQhYjUIu2o5v5u3rfJpCq5Cz0Q9WK--XdYxai_N2d0ImohPiIOp", synopsis: "Some text", year: 2011},
+  // Alternate syntax for getJSON
+  // $.getJSON('/api/movies', function(data) {
+  //   console.log(data);
+  // });
 
-  ];
+  $.getJSON('/api/movies')
+  .success(function(data) {
+    var movies = data;
+    movies.forEach(function(movie) {
+      movie.stringified = JSON.stringify(movie);
+    });
+    console.log("got data with promises");
+    var menuContent = $("#menu").html();
+    var menuTemplate = Handlebars.compile(menuContent)
 
-  var menuEl = Handlebars.compile($("#menu").html())({movies: movies});
-  var movieShowEl = Handlebars.compile($("#movie-show").html())({movie: movies[0]});
-  var movieEditEl = Handlebars.compile($("#movie-edit").html())({movie: movies[0]});
+    var movieShowTemplate = Handlebars.compile($("#movie-show").html());
+    var movieEditTemplate = Handlebars.compile($("#movie-edit").html());
 
-  mainPageEl.append(menuEl);
-  mainPageEl.append(movieShowEl);
-  // mainPageEl.append(movieEditEl);
+    mainPageEl.find(".menu").replaceWith(menuTemplate({movies: movies}));
+    // above selector is same as $("#main-page .menu")
+
+    $("ul li").click(function(e) {
+      // grab movie data from data-movie attribute
+      var movie = $(this).data("movie"); // use the jquery data attribute to get the movie
+      $("#main-page .content").replaceWith(movieShowTemplate({movie: movie}));
+    });
+  })
+  .error(function(data) {
+     console.log("something went wrong: ", data);
+  });
+
 });
